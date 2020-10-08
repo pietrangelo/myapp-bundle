ee766618372c4773d507328f60ee91a122e6ca13
Pietrangelo Masala <posta@pietrangelo.net>
2020-10-08 14:13:48 +0200

    Initial version of myapp generated by JHipster-6.9.0 with blueprints: entando-6.2.0

---
+++ src/main/java/net/pietrangelo/myapp/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package net.pietrangelo.myapp.config;
---
+++ src/main/java/net/pietrangelo/myapp/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package net.pietrangelo.myapp.web.rest.vm;
